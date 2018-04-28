19ecb31e0fc589ca494d962492bad76942f0155d
frank.binder <frank.binder@serialize.de>
2018-04-28 14:28:26 +0200

    Rename to UpperCase ProjectName

---
+++ src/LoxNET.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoxNet.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LoxNET.CLI");
+        }
+    }
+}
