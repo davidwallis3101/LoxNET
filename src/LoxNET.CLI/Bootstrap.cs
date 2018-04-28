cff4b7b2ae54658c2f85d77cea7555d1584681ad
frank.binder <frank.binder@serialize.de>
2018-04-28 14:56:17 +0200

    Bootstrap added to CLI

---
+++ src/LoxNET.CLI/Bootstrap.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using EventFlow;
+using EventFlow.Aggregates;
+using EventFlow.Configuration;
+using EventFlow.Core;
+using LoxNET.Transport;
+using LoxNET.Transport.Domain.Services;
+using LoxNET.Transport.Domain.Model.ClientModel;
+using LoxNET.Transport.Domain.Model.ClientModel.ValueObjects;
+using EventFlow.Extensions;
+using EventFlow.Logs;
+
+namespace LoxNET.CLI
+{
+    public class Bootstrap : IDisposable
+    {
+        private IRootResolver _resolver;
+        private IAggregateStore _aggregateStore;
+        private ICommandBus _commandBus;
+
+        public Bootstrap()
+        {
+           _resolver = EventFlowOptions.New
+                .ConfigureTransportDomain()
+                .UseConsoleLog()
+                .CreateResolver();
+
+            _aggregateStore = _resolver
+                .Resolve<IAggregateStore>();
+
+            _commandBus = _resolver
+                .Resolve<ICommandBus>();     
+
+
+
+       
+        }        
+        public Task LogException(string message)
+        {
+            var log = _resolver.Resolve<ILog>();
+            log.Debug(message);
+
+            return Task.FromResult(0);
+        }
+        public async Task Setup()
+        {
+
+            try
+            {
+                await CreateClientAggregateAsync().ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                await LogException(ex.Message).ConfigureAwait(false);
+            }
+
+            var svc = _resolver.Resolve<ISocketService>();
+
+        }
+
+
+        public Task CreateClientAggregateAsync()
+        {
+
+            Client client = new Client(
+                ClientId.New,
+                new Endpoint("LoxoneTestServer", "testminiserver.loxone.com", 7777),
+                new Credentials("Web", "Web")
+            );
+
+            return UpdateAsync<ClientAggregate, ClientId>(
+                client.Id, 
+                a => a.Initialize(
+                    client.Endpoint,
+                    client.Credentials
+                )
+            );
+        }
+
+
+
+        private async Task UpdateAsync<TAggregate, TIdentity>(TIdentity id, Action<TAggregate> action)
+            where TAggregate : IAggregateRoot<TIdentity>
+            where TIdentity : IIdentity
+        {
+            await _aggregateStore.UpdateAsync<TAggregate, TIdentity>(
+                id,
+                SourceId.New,
+                (a, c) =>
+                    {
+                        action(a);
+                        return Task.FromResult(0);
+                    },
+                CancellationToken.None)
+                .ConfigureAwait(false);
+        }
+
+
+
+        public void Dispose()
+        {
+            _resolver.DisposeSafe(new ConsoleLog(), "Disposed");
+        }
+    }
+}
--- src/LoxNET.CLI/Program.cs
+++ src/LoxNET.CLI/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using EventFlow;
+using EventFlow.Aggregates;
+using EventFlow.Configuration;
+using EventFlow.Core;
+using LoxNET.Transport;
+using LoxNET.Transport.Domain.Services;
+using LoxNET.Transport.Domain.Model.ClientModel;
+using LoxNET.Transport.Domain.Model.ClientModel.ValueObjects;
+using EventFlow.Extensions;
+using EventFlow.Logs;
 
 namespace LoxNET.CLI
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             Console.WriteLine("LoxNET.CLI");
+
+            var bootstrap = new Bootstrap();
+
+
+            new TaskFactory().StartNew(
+                async () => await bootstrap.Setup()
+            );
+
+            bootstrap.Dispose();
         }
     }
 }
