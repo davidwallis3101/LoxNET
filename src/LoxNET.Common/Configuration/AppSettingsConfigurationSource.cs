6a5ee27e1d4e6147f4e16889a92e6b9a58766875
Frank <github@serialize.eu>
2018-05-21 02:55:53 +0200

    AppSettingsSource added

---
+++ src/LoxNET.Common/Configuration/AppSettingsConfigurationSource.cs
@@ -0,0 +1,15 @@
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.Configuration.Binder;
+
+namespace LoxNET.Configuration
+{
+    public class AppSettingsConfigurationSource : IConfigurationSource
+    {
+
+    }
+}
